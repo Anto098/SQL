a0ae9c0543f52198591f30c1fd8c8f428932dcc8
Anto <antonin.roy1@gmail.com>
2021-05-13 17:03:12 -0400

    added median.sql

---
+++ median.sql
@@ -0,0 +1,13 @@
+/* LAT_N : param to get median of, STATION : table name*/
+SET @n := 0;
+SELECT COUNT(*) FROM STATION INTO @total;
+SELECT 
+    ROUND(AVG(S.LAT_N),4)
+FROM (SELECT @n := @n+1 AS row_index, LAT_N FROM STATION ORDER BY LAT_N) S
+WHERE
+    CASE 
+        WHEN MOD(@TOTAL, 2) = 0 
+            THEN S.row_index IN (@total/2, @total/2+1)
+            ELSE S.row_index = (@total+1)/2
+    END
+;
